52fed23a674780078e1935ebc3762c8135ce2acc
berri3 <cmliang2004@hotmail.com>
2017-12-06 22:44:13 -0500

    Changed file names for good

---
+++ Assignment6/src/Main.c
@@ -0,0 +1,52 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void producer();
+void consumer();
+
+int main() {
+
+	//variables
+	int pid;
+	FILE *TURN;
+
+	//initialize TURN to 0 before forking
+	TURN = fopen("TURN.txt", "wt");
+	if (TURN == NULL) {
+		printf("Main.c could not open the turn file! :(\n");
+		exit(1);
+	}
+	fputc('0', TURN);
+	fclose(TURN);
+
+
+
+	pid = fork();
+	//Fork starts here
+
+	//something went wrong, terminate
+	if (pid == -1){
+		printf("Something went wrong... :(\n");
+		exit(1);
+	}
+
+	//producer will do its job here
+	if (pid == 0) {
+		producer();
+
+	}
+
+	//consumer will do its job here
+	if (pid != 0) { 
+		consumer();
+	}
+
+	
+
+	return 0;
+}
+
